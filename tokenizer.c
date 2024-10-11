c2143803d87457ffe40c12d4a272bf316bc0239a
= <mzwart@f0r5s9.codam.nl>
2024-10-11 19:15:06 +0200

    can split words at delimiters

--- tokenizer.c
+++ tokenizer.c
@@ -17,110 +17,95 @@
 #include <stdio.h>
 #include <string.h>
 
+// TODO: make it possible for heredoc to set a word as END delimiter...
+enum e_identifiers
+{
+	SPACE = ' ',
+	ENV = '$',
+	EXIT_STATUS = '?',
+	PIPE = '|',
 
-struct tokenizer {
-	char	*input;
+	REDIRECT_IN = '<',
+	REDIRECT_OUT = '>',
+	REDIRECT_APPEND = 'a',
+
+	WORD,
+	NEW_LINE = '\n',
+	END = '\0',
+	HEREDOC_END,
+};
+
+const static	char	DELIMITERS[3] = {
+	' ', '\n', '\0'
 };
 
 typedef struct s_token {
-	// TODO: make it possible for heredoc to set a word as END delimiter...
-	enum e_identifiers
-	{
-		SPACE = ' ',
-		ENV = '$',
-		EXIT_STATUS = '?',
-
-
-		REDIRECT_IN,
-		REDIRECT_OUT,
-		REDIRECT_APPEND,
-
-		END = 0,
-		HEREDOC_END = 0, // maybe unnecessary
-	};
-	char	*text;
-	ssize_t	length;
-	bool	valid;
+	char			*text;
+	ssize_t			length;
+	enum e_identifiers	identifier;
 }	t_token;
 
-void	init_tokenizer(struct tokenizer *tokenizer, char *data)
+
+struct s_tokenizer {
+	char	*input;
+};
+
+//=============================================================================
+
+// NOTE: makes a shallow copy...
+void	init_tokenizer(struct s_tokenizer *tokenizer, char *data)
 {
 	tokenizer->input = data;
 }
 
-const static	char	delimiters[3] = {
-	' ', '\0', '\n'
-};
-
-
-
-// NOTE:
-// all tokens of length 1 are identifiers...
-
-t_token	tokenize_one_token(struct tokenizer *tokenizer)
+t_token	tokenize_one_token(struct s_tokenizer *tokenizer)
 {
-	t_token		token;
-	char		*string;
-	int		len;
+	char	*str;
+	t_token	token;
 
-	string = tokenizer->input;
-	token.literal_value = string;
-	token.valid = true;
-	len = 0;
-	while (*string && !strchr(delimiters, *string))
+	str = tokenizer->input;
+	token.text = NULL;
+	token.identifier = END;
+	token.length = 0;
+	while (*str && strchr(DELIMITERS, *str) != NULL)
+		++str;
+	if (*str == '\0')
+		return (token);
+	token.text = str;
+	while (*str && strchr(DELIMITERS, *str) == NULL)
 	{
-		++string;
-		++len;
+		++str;
+		++token.length;
 	}
-	if (len == 0)
+	if (token.length == 0 && *str == '\0')
+		return (token);
+	else if (token.length == 0)
 	{
-		token.literal_value = 0;
-		token.identifier = *string;
-		token.length = 1;
-	}
-	else if (strncmp(string, "$?", 2) == 0)
-	{
-		token.identifier = EXIT_STATUS;
+		token.text = 0;
+		token.identifier = *str;
 		token.length = 1;
 	}
 	else
-		token.length = len;
-	tokenizer->input += token.length;
+		token.identifier = WORD;
+	tokenizer->input = str;
 	return (token);
 }
 
 int main()
 {
-	struct tokenizer tokenizer;
-
-	char *str = "test .... string";
+	struct s_tokenizer tokenizer;
+	char	*str = strdup(" \n\nhello\n world!");
 	t_token		token;
 
 	init_tokenizer(&tokenizer, str);
+	printf("untokenized str: %s\n", tokenizer.input);
 
-	printf("untokenized string: %s\n", tokenizer.input);
-
-	token = tokenize_one_token(&tokenizer);
-	printf("%zu\n", token.length);
-
-	printf("%.*s\n", (int)token.length, token.literal_value);
-	
-	printf("%s\n", tokenizer.input);
-
-	printf("%.*s\n", (int)token.length, token.literal_value);
-
-	token = tokenize_one_token(&tokenizer);
-
-	token = tokenize_one_token(&tokenizer);
-
-	printf("%.*s\n", (int)token.length, token.literal_value);
-	printf("%i", token.identifier);
-	
-	printf("%s\n", tokenizer.input);
-
-
-
-
-
-	return 0;
+	token.text = (char *)1;
+	while (token.text)
+	{
+		token = tokenize_one_token(&tokenizer);
+		if (token.text)
+			printf("[%.*s]", (int)token.length, token.text);
+	}
+	printf("\noriginal string:%s", str);
 }
