cfbc815992c0a10536663f01d85ce93cc59f4862
= <mzwart@f1r5s21.codam.nl>
2024-10-21 21:42:46 +0200

    Single quotes parse working + more stuf Going to make seperate branch again since commit log will start to bloat if I continue on this branch

---
+++ libft/build/bool/ft_isalpha.d
@@ -0,0 +1,1 @@
+build/bool/ft_isalpha.o: bool/ft_isalpha.c
---
+++ libft/build/bool/ft_isdecimal.d
@@ -0,0 +1,6 @@
+build/bool/ft_isdecimal.o: bool/ft_isdecimal.c include/libft/libft.h \
+  include/libft/bool.h
+
+include/libft/libft.h:
+
+include/libft/bool.h:
